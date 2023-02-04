21082962768cf61c5e1df51d571a10c1ffa87d50
Jan Polak <jan.p.polak@gmail.com>
2023-02-04 21:56:31 +0100

    test

---
+++ ConsoleApp3/ContactBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ContactBuilder
+    {
+        public List<Contact> AddContacts()
+        {
+            string userChoice;
+            var contacts = new List<Contact>();
+            long contactNumber = 0;
+            bool isCorrect = true;
+
+            do
+            {
+                Console.Write("Type in contact name: ");
+                var contactName = Console.ReadLine();
+
+                Console.Write("Type in contact number: ");
+
+
+                do
+                {
+                    if (isCorrect != long.TryParse(Console.ReadLine(), out contactNumber))
+                    {
+                        Console.Write("Phone number has to be an integer. Try again: ");
+                    }
+                    else if (contactNumber.ToString().Count() != 9)
+                    {
+                        Console.Write("The number should contain 9 digits. Try again: ");
+                    }
+                }while(isCorrect && contactNumber.ToString().Count() != 9);
+
+                
+
+                var contact = new Contact() { ContactName = contactName, ContactNumber = contactNumber };
+                contacts.Add(contact);
+
+                Console.Write("Do you want to add another contact? Type 'y' or 'n': ");
+                userChoice = Console.ReadLine();
+
+            } while (userChoice == "y");
+
+            return contacts;
+        }
+
+        public static void ShowContacts(List<Contact> contacts)
+        {
+            var sortedContants = contacts.OrderBy(x => x.ContactName).ToList();
+            foreach (Contact contact in sortedContants)
+            {
+                Console.WriteLine($"Name: {contact.ContactName}, number: {contact.ContactNumber}");
+            }
+        }
+
+        public static void FindContact(List<Contact> contacts)
+        {
+            string userChoice;
+
+            Console.Write("Do you want to find a contact? Type 'y' or 'n'?: ");
+            userChoice = Console.ReadLine();
+            if (userChoice == "y")
+            {
+                Console.Write("Type in contact name: ");
+                var userInput = Console.ReadLine();
+
+                var selectedItem = contacts.Where(x => x.ContactName == userInput);
+
+                foreach (var item in selectedItem)
+                {
+                    Console.WriteLine($"Name: {item.ContactName}, number: {item.ContactNumber}");
+                }
+                
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactBuilder addNewContact = new ContactBuilder();
+            var contacts = addNewContact.AddContacts();
+            ContactBuilder.ShowContacts(contacts);
+            ContactBuilder.FindContact(contacts);
+            
+        }
+    }
+}
